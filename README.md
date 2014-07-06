8a6643f447154b792d791e7a105b2f0fc21214e8
sagar87 <harald.voeh@gmail.com>
2014-07-06 22:59:18 +0200

    Many changes.

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 EasyBlot
 ========
 
+## Description
+
 EasyBlot scans a folder and searches for all files that meet set specifications. The script shows each picture, and asks the user to specify crop restrictions. After cropping the picture, the user is asked to annotate the blot. The user may set X- and Y-coordinates and the name of the lanes. Subsequently, the script uses the plot- and the text-function of the marplotlib library to annotate the plot. Finally, the user may save his work to predefined folder.
+
+Before:
+
+![](images/before.tif)
+
+After:
+
+![](images/after.tif)
 
 ## Versions
 
-*2014-07-03* Demo Video added
+**2014-07-06** Demo Video added, redesigned code.
 
-*2014-07-02* 0.1 EasyBlot can crop, annotate and save the blot.
+**2014-07-02** EasyBlot can crop, annotate and save the blot.
--- easy_blot.py
+++ easy_blot.py
@@ -14,46 +14,153 @@ for root, dirs, files in os.walk('/Volumes/Daten/Dropbox/Bachelor Results/'):
         if '.tif' in _file and 'WB' in root:
             blots.append(root+ '/' +_file)
 
+def getBlotDimension(blot):
+    '''
+    Takes a blot as input and returns X- and Y-directions
+    '''
+    lx, ly, lz = blot.shape
+    return lx, ly
+
+def getBlotName(blot):
+    return blot.split('/')[-1]
+
+def getBlotDate(blot):
+    filename = getBlotName(blot)
+    return filename[0:10]
+
+
+def showBlot(blot):
+    '''
+    Takes a blot as input.
+
+    Returns a enlarged image of the blot.
+    '''
+    #xmax, ymax = getBlotDimension(blot)
+    plt.figure(figsize=(6*2,4*2))
+    imgplot = plt.imshow(blot, cmap=plt.cm.gray)
+    plt.grid(True)
+    plt.xticks(range(0, 600, 25), rotation=25)
+    plt.yticks(range(0, 400, 25))
+    plt.show()
+
+def cropBlot(blot, X1, Y1):
+    '''
+    Takes a blot and X1, Y1 coordinates as input. Based on the coordinates,
+    cropBlot will set X2 and Y2 automatically. The user may set the value that
+    is added to X1 and Y2 in the following lines
+
+    Returns the blot as a resized matrix.
+    '''
+    X2 = int(X1) + 400
+    Y2 = int(Y1) + 150
+
+    croppedBlot = blot[int(Y1):int(Y2), int(X1):int(X2)]
+
+    return croppedBlot
+
+def setBar(blot, X1, X2, Y):
+
+    X3 = int(X2)+10
+    X4 = X3+(int(X2)-int(X1))
+    imgplot = plt.imshow(blot, cmap=plt.cm.gray)
+    plt.autoscale(enable=False, axis='both')
+    plt.plot([X1, X2], [Y, Y], 'k-', lw=2)
+    plt.plot([X3, X4], [Y, Y], 'k-', lw=2)
+
+
+
+def isOk():
+    '''
+    Simple function that return True if the user is satisfied with his work.
+    '''
+    ok = raw_input('OK? ')
+    if ok == 'y':
+        return True
+    else:
+        return False
+
 
 for blot in blots:
     im = misc.imread(blot)
-    lx, ly, lz = im.shape
-    plt.figure(figsize=(6*2,4*2))
-    imgplot = plt.imshow(im[0:lx, 0:ly], cmap=plt.cm.gray)
-    plt.grid(True)
-    plt.xticks(range(75, 600, 50))
-    plt.show()
-    print 'image dimensions: %d, %d' % (lx, ly)
-    crop = raw_input('Set Cropping dimension, separate by commas (x1, x2, y1, y2):')
-    x1, y1 = crop.split(', ')
-    x2 = int(x1) + 400
-    y2 = int(y1) + 150
-    plt.figure(figsize=(6*1.5,4*1.5))
-    imgplot = plt.imshow(im[int(y1):int(y2), int(x1):int(x2)], cmap=plt.cm.gray)
-    plt.grid(True)
-    plt.show()
-    print blot.split('/')[-1]
-    annotation = raw_input('Annotate the blot? ')
-    if annotation=='y':
-        question = raw_input('Set X1, X2, and Y:')
-        bands = raw_input('What is on the lanes: ')
-        band1, band2 = bands.split(', ')
-        X1, X2, Y = question.split(', ')
-        X3 = int(X2)+10
-        X4 = X3+(int(X2)-int(X1))
-        plt.figure(figsize=(6*1.5,4*1.5))
-        imgplot = plt.imshow(im[int(y1):int(y2), int(x1):int(x2)], cmap=plt.cm.gray)
-        plt.grid(True)
-        plt.autoscale(enable=False, axis='both')
-        plt.plot([X1, X2], [Y, Y], 'k-', lw=2)
-        plt.plot([X3, X4], [Y, Y], 'k-', lw=2)
-        plt.text(((int(X2)-int(X1))/2), int(Y)+10, band1, style='italic', bbox={'facecolor':'white', 'alpha':0.5, 'pad':10})
-        plt.text(np.median(range(X3, X4)), int(Y)+10, band2, style='italic', bbox={'facecolor':'white', 'alpha':0.5, 'pad':10})
-        plt.show()
+    showBlot(im)
+    getBlotDimension(im)
+
+    print getBlotName(blot)
+
+    cropProcedure = False
+    annotateProcedure = False
+
+    while cropProcedure != True:
+        crop = raw_input('Set Cropping dimension, separate by commas (x1, y1):')
+        X1, Y1 = crop.split(', ')
+        croppedBlot = cropBlot(im, X1, Y1)
+        showBlot(croppedBlot)
+        cropProcedure = isOk()
+
+    #print blot.split('/')[-1]
+    while annotateProcedure != True:
+        print getBlotName(blot)
+        annotation = raw_input('Annotate the blot? ')
+        if annotation=='y':
+            question = raw_input('Set X1, X2, and Y:')
+            bands = raw_input('What is on the lanes? Antibody?: ')
+
+            band1, band2, ak = bands.split(', ')
+            X1, X2, Y = question.split(', ')
+            X3 = int(X2)+10
+            X4 = X3+(int(X2)-int(X1))
+
+            imgplot = plt.imshow(croppedBlot, cmap=plt.cm.gray)
+
+            plt.grid(True)
+            plt.autoscale(enable=False, axis='both')
+
+            # Set the bars
+
+            plt.plot([X1, X2], [Y, Y], 'k-', lw=2)
+            plt.plot([X3, X4], [Y, Y], 'k-', lw=2)
+
+            # Show text
+
+            plt.text(((int(X2)-int(X1))/2), int(Y)+20, band1, style='italic', bbox={'facecolor':'white', 'alpha':0.7, 'pad':10})
+            plt.text(((int(X4)-int(X3))/2)*3, int(Y)+20, band2, style='italic', bbox={'facecolor':'white', 'alpha':0.7, 'pad':10})
+
+            plt.title(getBlotDate(blot) + ' ' + str(ak))
+
+            plt.show()
+
+            annotateProcedure = isOk()
+
+    print getBlotName(blot)
     user = raw_input('Want to save (y or n)? ')
+
     if user == 'y':
         savepath= '/Volumes/Daten/Dropbox/Bachelor Results/' + blot.split('/')[-1]
-        plt.imshow(im[int(y1):int(y2), int(x1):int(x2)], cmap=plt.cm.gray)
+
+        imgplot = plt.imshow(croppedBlot, cmap=plt.cm.gray)
+
+        #plt.grid(True)
+        plt.autoscale(enable=False, axis='both')
+
+        # Set the bars
+
+        plt.plot([X1, X2], [Y, Y], 'k-', lw=2)
+        plt.plot([X3, X4], [Y, Y], 'k-', lw=2)
+
+        # Show text
+
+        plt.text(((int(X2)-int(X1))/2), int(Y)+20, band1, style='italic', bbox={'facecolor':'white', 'alpha':0.7, 'pad':10})
+        plt.text(((int(X4)-int(X3))/2)*3, int(Y)+20, band2, style='italic', bbox={'facecolor':'white', 'alpha':0.7, 'pad':10})
+
+        # Get rid of the x and y-axis
+
+        plt.axis('off')
+        #plt.tick_params(which='both', bottom='off', top='off', labelbottom='off')
+        #plt.tick_params(axis='y', which='both', bottom='off', top='off', labelbottom='off')
+
+        plt.title(getBlotDate(blot) + ' ' + str(ak))
+
         plt.savefig(savepath)
+
     elif user=='n':
         break
